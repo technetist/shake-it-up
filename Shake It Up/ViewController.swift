0c09d165f91e817bac651dcbf379da932d20e082
Adrien Maranville <adrien.maranville@gmail.com>
2017-03-16 00:30:56 -0600

    added audio files and implemented shaking to play a random sound. Sounds will play regardless of mute status.

--- Shake It Up/ViewController.swift
+++ Shake It Up/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    
+    var player = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+        
 
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+            
+            let soundArray = ["bass_bump", "snare_hit", "whistle", "fast_beat", "plucking"]
+            let randomNum = Int(arc4random_uniform(UInt32(soundArray.count)))
+            
+            let audioPath = Bundle.main.path(forResource: soundArray[randomNum], ofType: "wav")
+            
+            do {
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+                player.play()
+            }
+            catch {
+                
+            }
+
+            
+        }
+    }
 
 }
 
